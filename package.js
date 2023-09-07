e8c6b54145515402cb5866997ff8d8ae37ec0c2e
Jan Pilgenröder <pille@mac.com>
2023-09-07 15:27:29 +0200

    update packages

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'janmp:sdui',
-  version: '2.0.0',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Some high level React components and setup for backend.',
   // URL to the Git repository containing the source code for this package.
@@ -15,13 +15,12 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('coffeescript@2.7.0');
   api.use('typescript');
-  api.use('zodern:types');
+  api.use('zodern:types@1.0.9');
   api.use('reactive-var');
   api.use('alanning:roles@3.4.0');
-  api.use('mdg:validated-method');
-  api.use('peerlibrary:reactive-publish');
-  api.use('tunguska:reactive-aggregate');
-  api.use('janmp:chatterbrain');
+  api.use('mdg:validated-method@1.2.0');
+  api.use('peerlibrary:reactive-publish@0.10.0');
+  api.use('tunguska:reactive-aggregate@1.3.7');
   // api.use('mizzao:user-status@1.0.1'); 
   api.mainModule('sdui-client-dynamic.coffee', 'client');
   api.mainModule('sdui-server.coffee', 'server');
