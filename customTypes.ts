81074c202a8e83207c25152652a7d889b9d110f9
laurinbrechter <brechterlaurin@gmail.com>
2024-06-24 15:18:01 +0200

    generate types in new file.

---
+++ customTypes.ts
@@ -0,0 +1,183 @@
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2'
+import {Mongo} from 'meteor/mongo'
+import SimpleSchema from 'meteor/aldeed:simple-schema'
+import {FC} from 'react'
+
+export interface RoleObject {
+  role:  string | Array<string>
+  scope?: string
+  forAnyScope?: boolean
+}
+
+export type Role = string | Array<string> | RoleObject | ((id: string) => boolean)
+
+export interface createTableDataAPIParams {
+  sourceName: string
+  sourceSchema: SimpleSchema
+  collection: Mongo.Collection<object>
+  useObjectIds?: boolean
+  listSchema?: SimpleSchema
+  formSchema?: SimpleSchema
+  queryEditorSchema?: SimpleSchema
+  canEdit?: boolean
+  canSearch?: boolean
+  canUseQueryEditor?: boolean
+  canSort?: boolean
+  canAdd?: boolean
+  canDelete?: boolean
+  canExport?: boolean
+  viewTableRole?: Role
+  editRole?: Role
+  addRole?: Role
+  deleteRole?: Role
+  exportTableRole?: Role
+  query?: object
+  initialSortColumn?: string
+  initialSortDirection?: 'ASC' | 'DESC'
+  perLoad?: number
+  getPreSelectPipeline?: ({pub}?: {pub: object}) => Promise<Array<Object> | null>
+  getProcessorPipeline?: ({pub}?: {pub: object}) => Promise<Array<Object> | null>
+  getRowsPipeline?:
+    (_: {
+        pub: object,
+        search: string,
+        query?: Mongo.Query<any>,
+        sort?: Mongo.SortSpecifier
+        limit?: number
+        skip?: number}) => Array<object>
+  getExportPipeline?:
+    (options: {
+      search: string,
+      query?: Mongo.Query<any>,
+      sort?: Mongo.SortSpecifier}) => Array<object>
+  makeFormDataFetchMethodRunFkt?:
+    (options: {
+      collection: Mongo.Collection<any>
+      transFormIdToMongo: (id: any) => any
+      transFormIdToMiniMongo: (id: any) => any
+    }) => (options: {id: string}) => Mongo.Cursor<any>
+  makeSubmitMethodRunFkt?:
+    (options: {
+      collection: Mongo.Collection<any>
+      transFormIdToMongo: (id: any) => any
+      transFormIdToMiniMongo: (id: any) => any
+    }) => (options: {data: object, id: string}) => void
+  makeDeleteMethodRunFkt?:
+    (options: {
+      collection: Mongo.Collection<any>
+      transFormIdToMongo: (id: any) => any
+      transFormIdToMiniMongo: (id: any) => any
+    }) => (options: {id: string}) => void
+  noAutomaticObserver?: boolean
+  debounceDelay?: number
+  getObservers?:() => Array<any>
+  setupNewItem?: () => object
+  onSubmit?: (object) => any
+  checkDisableEditForRow?: boolean
+  checkDisableDeleteForRow?: boolean 
+}
+export interface createTableDataAPIReturn {
+  sourceName: string
+  listSchemaBridge: SimpleSchema2Bridge
+  formSchemaBridge: SimpleSchema2Bridge
+  queryEditorSchemaBridge: SimpleSchema2Bridge
+  rowsCollection: Mongo.Collection<any>
+  canEdit?: boolean
+  canSearch?: boolean
+  canUseQueryEditor?: boolean
+  canSort?: boolean
+  canAdd?: boolean
+  canDelete?: boolean
+  deleteConfirmation?: string
+  canExport?: boolean
+  viewTableRole?: string | Array<string>
+  editRole?: string | Array<string>
+  addRole?: string | Array<string>
+  deleteRole?: string | Array<string>
+  exportTableRole?: string | Array<string>
+  setupNewItem?: () => object
+  onSubmit?: (object) => any
+  onDelete?: ({id}: {id: string}) => Promise<any>
+  onChangeField?: ({_id, changeData}: {_id: string, changeData: object}) => any
+  query?: object
+  initialSortColumn?: string
+  initialSortDirection?: 'ASC' | 'DESC'
+  perLoad: number
+}
+
+export declare function createTableDataAPI(options: createTableDataAPIParams): createTableDataAPIReturn
+
+
+// This is for additional options we can shove into our Components
+export interface additionalDataTableOptions {
+  onRowClick?: ({rowData, index}: {rowData: any, index: number}) => void
+  autoFormChildren?: [any]
+  formDisabled?: boolean
+  formReadOnly?: boolean
+  loadEditorData?: ({id}: {id: string}) => Promise<any>
+  queryUiObject?: object
+}
+
+export type DataTableOptions = createTableDataAPIReturn & additionalDataTableOptions
+
+export interface additionalDataTableDisplayOptions {
+  rows: [any]
+  loadMoreRows: ({startIndex, stopIndex}: {startIndex: number, stopIndex: number}) => Promise<any>
+  sortColumn: string
+  sortDirection: 'ASC' | 'DESC'
+  onChangeSort: ({sortColumn, sortDirection}: {sortColumn: string, sortDirection: 'ASC' | 'DESC'}) => void
+  search: 'string'
+  onChangeSearch: (searchString: string) => void
+  onDelete: ({id}: {id: string}) => Promise<any>
+  onChangeQueryUiObject: (queryUiObject: object) => void 
+  mayAdd?: boolean
+  onAdd?: () => void
+  mayDelete?: boolean
+  mayEdit?: boolean
+  mayExport?: boolean
+  onExportTable?: () => void
+  isLoading?: boolean
+  overscanRowCount?: number
+  customComponents: customComponents
+}
+
+export type DataTableDisplayOptions = DataTableOptions & additionalDataTableDisplayOptions
+
+export declare function DataTableDisplayComponent(options: DataTableDisplayOptions): FC
+
+export interface DataTableHeaderOptions {
+  listSchemaBridge: SimpleSchema2Bridge
+  queryEditorSchemaBridge: SimpleSchema2Bridge
+  loadedRowCount: number
+  canSearch?: boolean
+  search?: string
+  onChangeSearch?: (searchString: string) => void
+  canUseQueryEditor?: boolean
+  queryUiObject?: object
+  onChangeQueryUiObject: (queryUiObject: object) => void
+  canExport?: boolean
+  mayExport?: boolean
+  onExportTable?: () => void
+  canAdd?: boolean
+  mayAdd?: boolean
+  onAdd?: () => void
+  canSort?: boolean
+  sortColumn?: string
+  sortDirection?: 'ASC' | 'DESC'
+  onChangeSort:  ({sortColumn, sortDirection}: {sortColumn: string, sortDirection: 'ASC' | 'DESC'}) => void
+  AdditionalHeaderButtonsLeft?: FC
+  AdditionalHeaderButtonsRight?: FC
+  // query?: object
+  // onChangeQuery?: (query: object) => void
+}
+export declare function DefaultHeader(options: DataTableHeaderOptions): FC
+
+// TODO [TS] gather types of all implemented customComponent props
+export type customComponents = {[key: string]: FC}
+
+export interface MeteorTableDataHandlerOptions {
+  dataOptions: DataTableOptions
+  DisplayComponent: typeof DataTableDisplayComponent
+  customComponents: customComponents
+}
+export declare function MeteorTableDataHandler(options: MeteorTableDataHandlerOptions): FC
--- generateTypes.sh
+++ generateTypes.sh
@@ -4,15 +4,11 @@
 coffee --compile --output coffee-compiled .
 
 
-# Check if at least two arguments are provided (output file and folder)
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 output.d.ts folder"
-  exit 1
-fi
+cp customTypes.ts typeDeclarations.ts
 
 # Get the output file name and folder
-output_file=$1
-folder=$2
+output_file="typeDeclarations.ts"
+folder="coffee-compiled"
 
 # Check if the folder exists
 if [ ! -d "$folder" ]; then
@@ -28,11 +24,7 @@ find "$folder" -type f -name '*.d.ts' | while read -r input_file; do
   echo "// Content from: $input_file" >> "$output_file";
   input_file_without_extension="${input_file%.d.ts}"
   echo "export * from './$input_file_without_extension'" >> "$output_file"
-  # Replace 'export function' with 'export declare function' and append to the output file
-  # sed 's/export function/export declare function/g' "$input_file" >> "$output_file"
-  
   echo "\n" >> "$output_file"  # Add a newline for separation
-
 done
 
 echo "Combined .d.ts files into $output_file"
